d4c7f05a767b204bb3ee82141173bc3c70895137
TornadoTebbe <paul.tebbe@yahoo.com>
2022-10-24 13:16:27 +0200

    Csv hochladen und rumprobieren mit dem Decoder

--- src/Main.elm
+++ src/Main.elm
@@ -1,49 +1,112 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (div, text, input, button)
+import Csv
+import Csv.Decode
+import Html exposing (div, text, input, button, pre, Html)
 import Html.Events exposing (onClick, onInput)
 import String exposing (fromInt, toInt)
 import Debug exposing (log)
+import Http
 
 
-type Messages =
-    Add
-    | ChangedAddText String
-
-init = 
-    { value = 50
-    , firstName = "Paul"
-    , numberToAdd = 0}
-
-view model =
-    div [] [
-        text (fromInt model.value)
-        , div [][]
-        , input [ onInput ChangedAddText][]
-        , button [onClick Add][text "Add"]]
+type Status
+    = Success
+    | Loading
+    | Failure
 
 
-parseUserNumber
-update msg model =
+type alias Model =
+    { status : Status
+    , data : String
+    }
+
+
+type Msg
+    = GotText (Result Http.Error String)
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
     let
-        log1 = log "message" msg
-        log2 = log "model" model
+        data =
+            [ "Student_Behaviour"]
+        cmds =
+            List.map
+                (\x ->
+                    Http.get
+                        { url = "http://localhost:8000/Daten/" ++ x ++ ".csv"
+                        , expect = Http.expectString GotText
+                        }
+                )
+                data
     in
+    ( { status = Loading, data = "" }
+    , Cmd.batch cmds
+    )
 
+decodeStockDay : Csv.Decode.Decoder (( String, Maybe Float ) -> a) a
+decodeStockDay =
+    Csv.Decode.map
+        (\gender twelthMark ->
+            ( gender
+            , case String.toFloat twelthMark of
+                Just o ->
+                    Just o
+
+                Nothing ->
+                    Nothing
+            )
+        )
+        (Csv.Decode.andMap
+            (Csv.Decode.field "12th Mark" Ok)
+            (Csv.Decode.field "Gender" Ok)
+        )
+
+
+csvString_to_data : String -> List ( String, Maybe Float )
+csvString_to_data csvRaw =
+    Csv.parse csvRaw
+        |> Csv.Decode.decodeCsv decodeStockDay
+        |> Result.toMaybe
+        |> Maybe.withDefault []
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
     case msg of
-        Add -> 
-            {model | value = 70}
-        ChangedAddText theText->
-            { model | numberToAdd = toInt theText }
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( { model | status = Success, data = model.data ++ fullText }, Cmd.none )
+
+                Err _ ->
+                    ( { model | status = Failure }, Cmd.none )
 
 
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
 
 
+view : Model -> Html Msg
+view model =
+    case model.status of
+        Failure ->
+            text "I was unable to load your book."
+
+        Loading ->
+            text "Loading..."
+
+        Success ->
+            pre [] [ text model.data ]
+
+
+main : Program () Model Msg
 main =
-    Browser.sandbox
-        {
-            init = init
-            , view = view
-            , update = update
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
         }
