b9c990e3fb3b2b94d3ca4994420ad5967a74dc7d
TornadoTebbe <paul.tebbe@yahoo.com>
2022-11-13 13:13:21 +0100

    Weiter Versuche der Datenmanipulation mit R

--- Daten/Student_Behaviour_Daten_R.R
+++ Daten/Student_Behaviour_Daten_R.R
@@ -1,4 +1,17 @@
-Student_Behaviour_Data <- read.csv ("Daten\\Student_Behaviour.csv")
-Rubrik1 <- 0-30 minute
-result <- strtoi(Rubrik1)
-print(result)
+SB_Data <- read.csv("C:\\Users\\Paul\\OneDrive\\Dokumente\\GitHub\\ElmTest\\Daten\\Student_Behaviour.csv")
+table(SB_Data$daily.studing.time)
+
+SB_Data$daily.studing.time <- factor(SB_Data$daily.studing.time, 
+                                levels = c("0 - 30 minute", "30 - 60 minute",
+                                           "1 - 2 Hour", "2 - 3 hour", "3 - 4 hour",
+                                           "More Than 4 hour"))
+
+table(SB_Data$daily.studing.time)
+
+
+
+
+
+nrow(SB_Data)
+empty(SB_Data)
+is.null(SB_Data)
